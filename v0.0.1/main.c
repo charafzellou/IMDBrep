771ed08362888c06de04c85f4a9fe1e9ccd16bca
evilflash <evilflash@zik.dj>
2017-10-19 16:02:26 +0200

    REBOOT

--- v0.0.1/main.c
+++ v0.0.1/main.c
@@ -1,23 +1,10 @@
-#include "readInfo.h"
-#include "writeInfo.h"
-#include "parseJSON.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "curl/curl.h"
+#include "json-c/json.h"
 
-int main(void)
+int main()
 {
-    char buffer[BUFFER_SIZE];
-    memset(buffer, '\0', BUFFER_SIZE);
-
-    if(readInfo(buffer) == -1)
-        return EXIT_FAILURE;
-
-    if(writeInfo(buffer) == -1)
-        return EXIT_FAILURE;
-
-    parseJSON();
-
-    return EXIT_SUCCESS;
+    printf("Hello world!\n");
+    return 0;
 }
-
